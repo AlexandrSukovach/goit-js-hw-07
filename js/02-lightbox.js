2578ed66d90a0d94bbaf109d3b0b060e38cbd623
Aleksandr Sukovach <alexsukovach@gmail.com>
2021-12-01 20:44:07 +0200

    добавление галерей в task-01, task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createColorCardsMarkup(galleryItems);
+
+// ==========добавляем в разметку============
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+// ============создаем разметку в html===================
+
+function createColorCardsMarkup(galleryItems) {
+   // ==========деструктуризировали свойства из colors===========
+   return galleryItems
+      .map(({ preview, description, original }) => {
+         return `
+         <div class="gallery__item">
+         <a class="gallery__item" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="Image ${description}" />
+         </a>
+         </div>`;
+      })
+      // ==============сшиваем массив элементов в одну строку========
+      .join('');
+}
