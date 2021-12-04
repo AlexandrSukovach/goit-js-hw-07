babf7cbf857c4583f1e0ed9699c170b3a595b37a
Aleksandr Sukovach <alexsukovach@gmail.com>
2021-12-04 17:49:11 +0200

    изменено task-01 и добавлено task-03

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,33 +1,55 @@
 import { galleryItems } from './gallery-items.js';
-
 // Change code below this line
 
-
-console.log(galleryItems);
 
 const galleryContainer = document.querySelector('.gallery');
 const cardsMarkup = createColorCardsMarkup(galleryItems);
 
-// ==========добавляем в разметку============
+// ==========добавляем в разметку===========
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
 // ============создаем разметку в html===================
-
 function createColorCardsMarkup(galleryItems) {
+
    // ==========деструктуризировали свойства из colors===========
    return galleryItems
       .map(({ preview, description, original }) => {
          return `
          <div class="gallery__item" >
-         <a class="gallery__link" href="${original}" data-lightbox="roadtrip" title="Image ${description}" >
-         <img class="gallery__image" src="${preview}"
-         data-source="${original}" id ="gallery__image--img" alt="Image ${description}"/>
+         <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" width="509" height="340" alt="${description}"/>
          </a>
          </div>`;
       })
       // ==============сшиваем массив элементов в одну строку========
       .join('');
+}
+// =============выбираем картинку=============
+galleryContainer.addEventListener('click', onClickReset);
+
+function onClickReset(evennts) {
+   // ==========убираем перезагрузку=========== 
+   evennts.preventDefault();
+   if (evennts.target.nodeName !== "IMG") {
+      return
+   };
+   console.log(evennts.target)
+
+   let clickImage = evennts.target;
+   let originalImages = "";
+
+   galleryItems.forEach((img) => {
+      if (clickImage.src === img.preview) {
+         originalImages = img.original;
+      }
+   });
+
+   // ===============из мануала basiclightbox===================
+   document.querySelector('.gallery').onclick = () => {
+      basicLightbox.create(`
+   <img class="gallery__image" width="1400" height="900"  src="${originalImages}">
+	`).show()
+
+   };
 };
-
-
 
--- js/03-lightbox-2.js
+++ js/03-lightbox-2.js
@@ -1,26 +1,38 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 
 const galleryContainer = document.querySelector('.gallery');
 const cardsMarkup = createColorCardsMarkup(galleryItems);
 
-// ==========добавляем в разметку===========
+galleryContainer.addEventListener('click', onClickReset);
+
+function onClickReset(event) {
+   event.preventDefault();
+}
+
+
+// ==========добавляем в разметку============
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
 // ============создаем разметку в html===================
-function createColorCardsMarkup(galleryItems) {
 
+function createColorCardsMarkup(galleryItems) {
    // ==========деструктуризировали свойства из colors===========
    return galleryItems
       .map(({ preview, description, original }) => {
          return `
          <div class="gallery__item" >
-         <a class="gallery__link" href="${original}">
-         <img class="gallery__image" src="${preview}" width="509" height="340" alt="${description}"/>
+         <a class="gallery__link" href="${original}" data-lightbox="roadtrip" title="Image ${description}" >
+         <img class="gallery__image" src="${preview}"
+         data-source="${original}" id ="gallery__image--img" alt="Image ${description}"/>
          </a>
          </div>`;
       })
       // ==============сшиваем массив элементов в одну строку========
       .join('');
-}
+};
+
+
+
