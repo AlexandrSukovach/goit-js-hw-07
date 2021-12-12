abb0c6c9fac1afac8c71073b09dbd55e66a478cc
Aleksandr Sukovach <alexsukovach@gmail.com>
2021-12-12 13:53:36 +0200

    рефакторинг task-02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,7 +4,6 @@ import { galleryItems } from './gallery-items.js';
 const galleryContainer = document.querySelector('.gallery');
 const cardsMarkup = createColorCardsMarkup(galleryItems);
 galleryContainer.addEventListener('click', onClickReset);
-
 // ==========добавляем в разметку===========
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
@@ -24,54 +23,45 @@ function createColorCardsMarkup(galleryItems) {
       .join('');
 };
 
-//=================способ-1================
-//=============выбираем картинку=============
-function onClickReset(evennts) {
-
-   // ==========убираем перезагрузку===========
-   evennts.preventDefault();
+// =============способ-1================
+// =============выбираем картинку=============
+function onClickReset(event) {
+   // ==========убираем перезагрузку=========== 
+   event.preventDefault();
+   // console.dir(event.target);
    // =======убираем возможность кликать не по картинке======
-   if (evennts.target.nodeName !== "IMG") {
+   if (event.target.nodeName !== "IMG") {
       return
    };
    // ========ссылка на картинку=============
-   let clickImage = evennts.target;
-   let originalImages = "";
-   galleryItems.forEach((img) => {
-      if (clickImage.src === img.preview) {
-         originalImages = img.original;
-      }
-   });
-   // ===============из мануала basiclightbox===================
+   let originalImages = event.target.dataset['source'];
+   console.log(originalImages);
+   // ===============из мануала basiclightbox========
    basicLightbox.create(`
-   <img class="gallery__image" width="1300" height="900"  src="${originalImages}">
-   `).show()
+      <img class="gallery__image" width="1300" height="900"  src="${originalImages}">
+      `).show();
 };
 
+// //=================способ-2================
+// //=============выбираем картинку=============
+// function onClickReset(evennts) {
 
-
-
-
-
-
-
-
-
-
-
-// // =============способ-2================
-// // =============выбираем картинку=============
-// function onClickReset(event) {
-//    // ==========убираем перезагрузку=========== 
-//    event.preventDefault();
-//    // console.dir(event.target);
+//    // ==========убираем перезагрузку===========
+//    evennts.preventDefault();
+//    // =======убираем возможность кликать не по картинке======
+//    if (evennts.target.nodeName !== "IMG") {
+//       return
+//    };
 //    // ========ссылка на картинку=============
-//    let originalImages = event.target.dataset['source'];
-//    // console.log(originalImages);
-//    // ===============из мануала basiclightbox========
+//    let clickImage = evennts.target;
+//    let originalImages = "";
+//    galleryItems.forEach((img) => {
+//       if (clickImage.src === img.preview) {
+//          originalImages = img.original;
+//       }
+//    });
+//    // ===============из мануала basiclightbox===================
 //    basicLightbox.create(`
-//       <img class="gallery__image" width="1300" height="900"  src="${originalImages}">
-//       `).show();
+//    <img class="gallery__image" width="1300" height="900"  src="${originalImages}">
+//    `).show()
 // };
-
-
